a3db92be510d72790cd3696129860ca695cf5a08
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-10-25 15:24:09 -0500

    Exported accountsRouter and imported into Server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const accountsRouter = require("../router/accountsRouter")
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", accountsRouter)
+
+server.get("/", (req, res) => {
+    res.status(200).json({ API: "The API is running."})
+})
 
 module.exports = server;
